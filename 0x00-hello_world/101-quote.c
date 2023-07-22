0632d01c80cc77330e0be0be29ebe8395a8522d9
Adel484 <tomasander12@gmail.com>
2023-07-22 09:12:15 -0700

    no printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	const char* message = and that piece of art is useful\" - Dpra Lpr[ar. 2015-10-19";
+	const char* message = "and that piece of art is useful\" - Dpra Lpr[ar. 2015-10-19";
 	size_t len = strlen(message);
 	write(1, message, len);
 	return (0);
