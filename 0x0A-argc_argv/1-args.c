336bb9e3750ecb70b40af40106d873a9c78b2c85
Adel484 <tomasander12@gmail.com>
2023-11-19 23:42:15 +0200

    number

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,7 +9,7 @@
  * Return: Always 0.
  */
 
-int main(int argc, char __attribute__((__unused__)) argv **argv)
+int main(int argc, char __attribute__((__unused__)) **argv)
 {
 	printf("%d\n", argc - 1);
 	return 0;
