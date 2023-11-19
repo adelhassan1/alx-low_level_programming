d890379e82e933437a171315188ac007bf64be38
Adel484 <tomasander12@gmail.com>
2023-11-19 23:25:09 +0200

    try

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,8 @@
  * Return: Always 0.
  */
 
-void main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return;
+	return 0;
 }
