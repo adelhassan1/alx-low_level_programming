74ee286d340cabb229918ab184baef72aa09df0f
Adel484 <tomasander12@gmail.com>
2023-12-03 19:10:11 +0200

    initializing variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,14 +16,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d;
-
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return (NULL);
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-	return (d);
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
