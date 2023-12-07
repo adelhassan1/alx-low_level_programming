dcaf3e92bf46ab7a719997805e2334b08ca2e9d3
Adel484 <tomasander12@gmail.com>
2023-12-07 23:11:36 +0200

    singly linked list

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+    hello_msg db 'Hello, Holberton', 0
+
+section .text
+    global _start
+
+_start:
+    ; Write the message to stdout
+    mov rax, 1        ; syscall number for sys_write
+    mov rdi, 1        ; file descriptor 1 (stdout)
+    mov rsi, hello_msg  ; pointer to the message
+    mov rdx, 13       ; message length
+    syscall          ; make syscall
+
+    ; Exit the program
+    mov rax, 60       ; syscall number for sys_exit
+    xor rdi, rdi      ; exit code 0
+    syscall          ; make syscall
+
