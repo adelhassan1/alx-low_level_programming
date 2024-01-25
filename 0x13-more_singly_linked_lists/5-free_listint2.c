68d294e4b960030f0592116d2f4998bb328c82e7
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-01-26 00:15:12 +0200

    tasks upload

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (!head)
+		return;
+
 	while (*head)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
-	free(head);
+	*head = NULL;
 }
