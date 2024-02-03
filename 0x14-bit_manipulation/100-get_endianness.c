60eefb2f8a49ecfea61fc781026eb5f742e0f18a
adelhassan1 <tomasander12@gmail.com>
2024-02-03 23:27:44 +0200

    bit

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - check endianess. Return 1 if little indian
+ * Return: 1 if true, 0 if false
+ *
+ */
+int get_endianness(void)
+{
+	int n;
+
+	n = 1;
+	if (*(char *)&n == 1)
+		return (1);
+
+	return (0);
+}
