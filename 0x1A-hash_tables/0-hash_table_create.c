adc93485780f062ed7024e92b29e72efd9128872
adelhassan1 <tomasander12@gmail.com>
2024-02-27 16:11:05 +0200

    hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ *
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	hash_node_t *node;
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+
+	table->array = malloc(sizeof(node) * size);
+	if (!(table->array))
+		return (NULL);
+	table->size = size;
+	return (table);
+}
